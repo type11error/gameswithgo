102e95d3321063faa84ec111c9039298474d5050
type11error <type11error@gmail.com>
2020-12-15 23:45:51 -0800

    Checkpoint EP10

--- pong/pong.go
+++ pong/pong.go
@@ -11,6 +11,8 @@ import (
   "fmt"
   "github.com/veandco/go-sdl2/sdl"
   "time"
+  "github.com/type11error/gameswithgo/noise"
+  "math"
 )
 
 const winWidth int = 800
@@ -160,7 +162,7 @@ var nums = [][]byte {
     return pos{float32(winWidth)/2, float32(winHeight)/2}
   }
 
-  func (paddle *paddle) update(keyState []uint8, elapsedTime float32) {
+  func (paddle *paddle) update(keyState []uint8, controllerAxis int16, elapsedTime float32) {
     if keyState[sdl.SCANCODE_UP] != 0 {
       paddle.y -= paddle.speed * elapsedTime
     }
@@ -194,6 +196,69 @@ var nums = [][]byte {
       pixels[index+2] = c.b
     }
   }
+
+  // calculate a value between two numbers given a percentage
+  func flerp(b1 byte, b2 byte, pct float32) byte {
+    return byte(float32(b1) + pct*(float32(b2)-float32(b1)))
+  }
+
+
+  // lerp for a color value
+  func colorLerp(c1, c2 color, pct float32) color {
+    return color{flerp(c1.r, c2.r, pct), flerp(c1.g, c2.g, pct), flerp(c1.b, c2.b, pct)}
+  }
+
+  // calculate a 256 gradient between two colors
+  func getGradient(c1, c2 color) []color {
+    result := make([]color, 256)
+    for i := range result {
+      pct := float32(i) / float32(255)
+      result[i] = colorLerp(c1, c2, pct)
+    }
+    return result
+  }
+
+  func getDualGradient(c1,c2,c3,c4 color) [] color {
+    result := make([]color, 256)
+    for i := range result {
+      pct := float32(i) / float32(255)
+      if pct < 0.5 {
+        result[i] = colorLerp(c1, c2, pct*float32(2))
+      } else {
+        result[i] = colorLerp(c3, c4, pct*float32(1.5)-float32(0.5))
+      }
+    }
+    return result
+  }
+
+  // make a value only between a min and max
+  func clamp(min, max, v int) int {
+    if v < min {
+      v = min
+    } else if v > max {
+      v = max
+    }
+
+    return v
+  }
+
+  func rescaleAndDraw(noise []float32, min, max float32, gradient []color, w, h int) []byte {
+    result := make([]byte,w*h*4)
+    scale := 255.0 / (max - min)
+    offset := min * scale
+
+    for i := range noise {
+      noise[i] = noise[i]*scale - offset
+      c := gradient[clamp(0, 255, int(noise[i]))]
+      p := i * 4
+      result[p] = c.r
+      result[p+1] = c.g
+      result[p+2] = c.b
+    }
+
+    return result
+  }
+
 
 
   func main() {
@@ -244,8 +309,13 @@ var nums = [][]byte {
 
     keyState := sdl.GetKeyboardState()
 
+    noise,min,max := noise.MakeNoise(noise.FBM, .01, 0.2,2,3,winWidth, winHeight)
+    gradient := getGradient(color{255,0,0}, color{0,0,0})
+    noisePixels := rescaleAndDraw(noise,min,max,gradient,winWidth, winHeight)
+
     var frameStart time.Time
     var elapsedTime float32
+    var controllerAxis int16
 
 
     running := true
@@ -262,12 +332,12 @@ var nums = [][]byte {
 
       for _,controller := range controllerHandlers {
         if controller != nil {
-          controllerAxis = controller.GetAxis(sdl.CONTROLLER_AXIS_LEFTY)
+          controllerAxis = controller.Axis(sdl.CONTROLLER_AXIS_LEFTY)
         }
       }
 
       if state == play {
-        player1.update(keyState, controllerAxis int16, elapsedTime)
+        player1.update(keyState, controllerAxis, elapsedTime)
         player2.aiUpdate(&ball, elapsedTime)
         ball.update(&player1, &player2, elapsedTime)
       } else if state == start {
@@ -280,7 +350,11 @@ var nums = [][]byte {
         }
       }
 
-      clear(pixels)
+      //clear(pixels)
+      for i := range noisePixels {
+        pixels[i] = noisePixels[i]
+      }
+
       player1.draw(pixels)
       player2.draw(pixels)
       ball.draw(pixels)
@@ -291,7 +365,7 @@ var nums = [][]byte {
 
       elapsedTime = float32(time.Since(frameStart).Seconds()) / 1000.0
       if elapsedTime < .005 {
-        sdl.Delay(5 - uint32(elapsedTime/1000.0))
+        sdl.Delay(5 - uint32(elapsedTime*1000.0))
         elapsedTime = float32(time.Since(frameStart).Seconds())
       }
     }
